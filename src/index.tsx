45a94991229c872410eb3d909391099e586d2d5b
smartfellah <smartfellah@outlook.com>
2023-02-21 15:29:45 +0300

    Add redux and dnd

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./components/app/app";
+import { Provider } from "react-redux";
+import { store } from "./services/create-store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
