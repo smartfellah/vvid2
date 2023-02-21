227ce21c633d19676939f7729f3e073900d092ec
smartfellah <smartfellah@outlook.com>
2023-02-21 15:00:26 +0300

    Remove unused

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./app.module.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
