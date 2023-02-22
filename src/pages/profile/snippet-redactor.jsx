e34b04d752940075120ee58c43e8470db2316892
smartfellah <smartfellah@outlook.com>
2023-02-22 17:13:16 +0300

    Add styling

--- src/pages/profile/snippet-redactor.jsx
+++ src/pages/profile/snippet-redactor.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, Fragment } from "react";
 import { apiRequest } from "../../utils/api-request";
 import { dataURL } from "../../utils/apiURL";
+import styles from "./styles/snippet-redactor.module.css";
 export function SnippetRedactor() {
   const [snippetData, setSnippetData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -23,16 +24,49 @@ export function SnippetRedactor() {
   }, []);
 
   return !isLoading ? (
-    <div>
-      <ul>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "var(--s-8)",
+          paddingBlock: "var(--s-8)",
+        }}
+      >
         {snippetData.map((snippet) => {
           return (
             <Fragment key={snippet.ID}>
-              <li>{snippet.title}</li>
+              <li
+                style={{
+                  fontFamily: "'Comfortaa', Arial, Helvetica, Sans-serif",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "8px",
+                  textAlign: "center",
+                  background: "var(--bg-grey)",
+                  borderTop: "solid 2px var(--bg-primary)",
+                  width: "1280px",
+                }}
+              >
+                <p style={{ padding: "var(--s-4)" }}> {snippet.title}</p>
+                <p style={{ padding: "var(--s-4)" }}>{snippet.code}</p>
+                <p style={{ padding: "var(--s-4)" }}>
+                  {snippet.platformType === 1 ? "Solana" : "Hyperledger Fabric"}
+                </p>
+              </li>
             </Fragment>
           );
         })}
       </ul>
     </div>
-  ) : null;
+  ) : (
+    <div className={`${styles["snippet-redactor_wrapper"]}`}>
+      <div className={`${styles["loader"]}`}></div>
+    </div>
+  );
 }
